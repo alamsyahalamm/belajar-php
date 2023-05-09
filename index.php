e4cb21adbb72d0d125376cbcaaf6c20db715a971
alamsyahalamm <muhammad728744@smk.belajar.id>
2023-05-09 08:52:00 +0700

    Upload PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar PHP</title>
+</head>
+<body>
+    <?php
+    echo "belajar";
+
+    echo "\n";
+
+    // tagap 2
+    $name = "belajar PHP menggunakanan var "
+    echo $name
+    ?>
+</body>
+</html>
